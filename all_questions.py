e67987a0deadd613a6b7b7dca1f6e7cae8c25a1a
shirishahechirla <156143385+shirishahechirla@users.noreply.github.com>
2024-02-15 22:42:55 -0500

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -23,48 +23,62 @@ def question1():
     level2_left = {}
     level2_right = {}
 
-    level1["smoking"] = 0.
-    level1["smoking_info_gain"] = 0.
+    level1["smoking"] = 1 # 0.72
+    level1["smoking_info_gain"] = 0.2781
 
-    level1["cough"] = 0.
-    level1["cough_info_gain"] = 0.
+    level1["cough"] = -1 # 0.97
+    level1["cough_info_gain"] = 0.0349
 
-    level1["radon"] = 0.
-    level1["radon_info_gain"] = 0.
+    level1["radon"] = -1 # 0.76
+    level1["radon_info_gain"] = 0.2365
 
-    level1["weight_loss"] = 0.0
-    level1["weight_loss_info_gain"] = 0.
+    level1["weight_loss"] = -1 # 0.97
+    level1["weight_loss_info_gain"] = 0.029
 
-    level2_left["smoking"] = 0.
-    level2_left["smoking_info_gain"] = 0.
-    level2_right["smoking"] = 0.
-    level2_right["smoking_info_gain"] = 0.
+    level2_left["smoking"] = -1
+    level2_left["smoking_info_gain"] = -1
+    level2_right["smoking"] = -1
+    level2_right["smoking_info_gain"] = -1
 
-    level2_left["radon"] = 0.
-    level2_left["radon_info_gain"] = 0.
+    level2_left["radon"] = 1
+    level2_left["radon_info_gain"] = 0.7219
 
-    level2_left["cough"] = 0.
-    level2_left["cough_info_gain"] = 0.
+    level2_left["cough"] = -1
+    level2_left["cough_info_gain"] = 0.3219
 
-    level2_left["weight_loss"] = 0.
-    level2_left["weight_loss_info_gain"] = 0.
+    level2_left["weight_loss"] = -1
+    level2_left["weight_loss_info_gain"] = 0.171
 
-    level2_right["radon"] = 0.
-    level2_right["radon_info_gain"] = 0.
+    level2_right["radon"] = 1
+    level2_right["radon_info_gain"] = 0.7219
 
-    level2_right["cough"] = 0.
-    level2_right["cough_info_gain"] = 0.
+    level2_right["cough"] = -1
+    level2_right["cough_info_gain"] = 0.3219
 
-    level2_right["weight_loss"] = 0.
-    level2_right["weight_loss_info_gain"] = 0.
+    level2_right["weight_loss"] = -1
+    level2_right["weight_loss_info_gain"] = 0.171
 
     answer["level1"] = level1
     answer["level2_left"] = level2_left
     answer["level2_right"] = level2_right
 
-    # Fill up `construct_tree``
+    # Fill up construct_tree`
     # tree, training_error = construct_tree()
-    tree = u.BinaryTree("root")  # MUST STILL CREATE THE TREE *****
+    tree = u.BinaryTree("smoking == Yes")  # MUST STILL CREATE THE TREE ***
+    A = tree.insert_left("cough = Yes")
+    B = tree.insert_right("randon == Yes")
+    A.insert_left("y")
+    A.insert_left("y")
+    A.insert_left("y")
+    A.insert_left("y")
+    A.insert_left("n")
+    B.insert_left("y")
+    B.insert_right("n")
+    B.insert_right("n")
+    B.insert_right("n")
+    B.insert_right("n")
+    # tree, training_error = construct_tree
+    # tree = U.BinaryTree("root")
     answer["tree"] = tree  # use the Tree structure
     # answer["training_error"] = training_error
     answer["training_error"] = 0.0  
@@ -103,18 +117,18 @@ def question3():
     answer = {}
 
     # float
-    answer["(a) Gini, overall"] = 0.
+    answer["(a) Gini, overall"] = 0.5
 
     # float
     answer["(b) Gini, ID"] = 0.0
-    answer["(c) Gini, Gender"] = 0.
-    answer["(d) Gini, Car type"] = 0.
-    answer["(e) Gini, Shirt type"] = 0.
+    answer["(c) Gini, Gender"] = 0.48
+    answer["(d) Gini, Car type"] = 0.1625
+    answer["(e) Gini, Shirt type"] = 0.491
 
-    answer["(f) attr for splitting"] = ""
+    answer["(f) attr for splitting"] = "Car type"
 
     # Explanatory text string
-    answer["(f) explain choice"] = ""
+    answer["(f) explain choice"] = "The attribute that is used for splitting after the root node is car type because it has the least Gini index."
 
     return answer
 
@@ -135,45 +149,45 @@ def question4():
     #  'quantitative', 'interval', 'ratio'
     # If you have a choice between 'binary' and 'discrete', choose 'binary'
 
-    answer["a"] = []
+    answer["a"] = ['binary', 'quantitative', 'interval']
 
     # Explain if there is more than one interpretation. Repeat for the other questions. At least five words that form a sentence.
     answer["a: explain"] = ""
 
-    answer["b"] = []
+    answer["b"] = ['continuous', 'quantitative', 'ratio']
     answer["b: explain"] = ""
 
-    answer["c"] = []
-    answer["c: explain"] = ""
+    answer["c"] = ['continuous', 'qualitative', 'ordinal']
+    answer["c: explain"] = "This can also be interval in quantitative classification"
 
-    answer["d"] = []
+    answer["d"] = ['continuous', 'quantitative', 'ratio']
     answer["d: explain"] = ""
 
-    answer["e"] = []
+    answer["e"] = ['discrete', 'qualitative', 'ordinal']
     answer["e: explain"] = ""
 
-    answer["f"] = []
+    answer["f"] = ['continuous', 'quantitative', 'ratio']
     answer["f: explain"] = ""
 
-    answer["g"] = []
+    answer["g"] = ['discrete', 'quantitative', 'ratio']
     answer["g: explain"] = ""
 
-    answer["h"] = []
+    answer["h"] = ['discrete', 'qualitative', 'nominal']
     answer["h: explain"] = ""
 
-    answer["i"] = []
+    answer["i"] = ['discrete', 'qualitative', 'nominal']
     answer["i: explain"] = ""
 
-    answer["j"] = []
+    answer["j"] = ['discrete', 'qualitative', 'ordinal']
     answer["j: explain"] = ""
 
-    answer["k"] = []
-    answer["k: explain"] = ""
+    answer["k"] = ['continuous', 'quantitative', 'ratio']
+    answer["k: explain"] = "it sometimes might be discrete when the measurement is like number of buildings away and it might be interval too"
 
-    answer["l"] = []
+    answer["l"] = ['continuous', 'quantitative', 'ratio']
     answer["l: explain"] = ""
 
-    answer["m"] = []
+    answer["m"] = ['discrete', 'qualitative', 'nominal']
     answer["m: explain"] = ""
 
     return answer
@@ -188,18 +202,18 @@ def question5():
     # Read appropriate section of book chapter 3
 
     # string: one of 'Model 1' or 'Model 2'
-    explain["a"] = ""
-    explain["a explain"] = ""
+    explain["a"] = "Model-2"
+    explain["a explain"] = "This is because model-2 is pruned version and it is less likely to overfit the data and have better performances on unseen data."
 
     # string: one of 'Model 1' or 'Model 2'
-    explain["b"] = ""
-    explain["b explain"] = ""
+    explain["b"] = "Model-1"
+    explain["b explain"] = "Even though Model 1 performed better on the combined dataset (A + B), Model 2 would be the better option for classification tasks because of its better generalization capabilities. Importantly for real-world applications, Model 2 is more likely to perform well on unknown situations and is less likely to overfit to the training data."
 
-    explain["c similarity"] = ""
-    explain["c similarity explain"] = ""
+    explain["c similarity"] = "Regularization"
+    explain["c similarity explain"] = "In order to aid in model selection and prevent overfitting, they both seek to incorporate model complexity into the loss function."
 
-    explain["c difference"] = ""
-    explain["c difference explain"] = ""
+    explain["c difference"] = "Criterion"
+    explain["c difference explain"] = "One difference between them is how they go about calculating model complexity, especially when it comes to decision trees."
 
     return explain
 
@@ -238,26 +252,26 @@ def question7():
     answer = {}
 
     # float
-    answer["a, info gain, ID"] = 0.
-    answer["b, info gain, Handedness"] = 0.
+    answer["a, info gain, ID"] = 1.0
+    answer["b, info gain, Handedness"] = 0.561
 
     # string: "ID" or "Handedness"
-    answer["c, which attrib"] = ""
+    answer["c, which attrib"] = "ID"
 
     # answer is a float
-    answer["d, gain ratio, ID"] = 0.
-    answer["e, gain ratio, Handedness"] = 0.
+    answer["d, gain ratio, ID"] = 'undefined'
+    answer["e, gain ratio, Handedness"] = 0.561
 
     # string: one of 'ID' or 'Handedness' based on gain ratio
     # choose the attribute with the largest gain ratio
-    answer["f, which attrib"] = ""
+    answer["f, which attrib"] = "Handedness"
 
     return answer
 
 
 # ----------------------------------------------------------------------
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     answers = {}
     answers["q1"] = question1()
     answers["q2"] = question2()
@@ -268,4 +282,3 @@ if __name__ == "__main__":
     answers["q7"] = question7()
 
     u.save_dict("answers.pkl", answers)
-
